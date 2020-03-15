db48fce3c547d5adf8a9304923d6d5683a6c7000
ANH NGUYEN <nqta_611@yahoo.com>
2020-03-15 14:56:45 -0700

    Change to use Mysql db instead of H2 db

--- src/test/java/com/example/accessingdatamysql/AccessingDataMysqlApplicationTests.java
+++ src/test/java/com/example/accessingdatamysql/AccessingDataMysqlApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.accessingdatamysql;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AccessingDataMysqlApplicationTests {
 
 	@Test
 	void contextLoads() {
